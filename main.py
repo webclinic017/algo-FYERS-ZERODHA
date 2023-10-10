130e184efcb47d10912defed1c60add68a1c2acd
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2023-10-11 00:50:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-import streamlit as st
-import time
+from flask import Flask
 
-st.sidebar.markdown(
-    "<h1 style='font-family: Comic Sans MS; color: #FF5733;'>Welcome to AlgoTrade</h1>",
-    unsafe_allow_html=True
-)
+app = Flask(__name__)
 
-st.sidebar.markdown(
-    """
-    <hr style='border: 2px solid white;'>
-    """,
-    unsafe_allow_html=True
-)
+@app.route('/count')
+def hello_world():
+    return 'Hello, World!'
 
-
-
-connect_ = st.sidebar.button('connect' , use_container_width=True)
-mode = st.sidebar.radio(':red[***Select the Mode of trading:***]',['***Simulation***','***Live***'])
-
-def countdown(seconds):
-    for i in range(seconds, 0, -1):
-        timer_placeholder.text(f"Time remaining: {i} seconds")
-        time.sleep(1)
-    timer_placeholder.text("Time's up!")
-
-# Create a placeholder for the countdown timer
-timer_placeholder = st.empty()
-
-# Set the countdown duration (e.g., 10 seconds)
-countdown_duration = 100000
-
-# Start the countdown
-countdown(countdown_duration)
+if __name__ == '__main__':
+    app.run()
