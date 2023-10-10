2ecba4e874270f99ef31d1cc41c94677d94e4c30
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2023-10-11 05:27:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template ,jsonify
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/count')
-def hello_world():
-    return 'Hello, World!'
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/update-tick-data')
+def update_tick_data():
+    # Simulated function to fetch new tick data (replace with actual API calls)
+    # In this example, we simply return the same simulated data for demonstration purposes
+    updated_data = {
+        'banknifty': np.random.randint(35000,44500),
+        'nifty':  np.random.randint(18000,20000),
+        'finnifty':  np.random.randint(18000,19000),
+    }
+    return jsonify(updated_data)
+
