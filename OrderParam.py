a2568b0701adb0e59f1e039b8d5097cfda132021
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2023-10-25 12:42:54 +0530

    Update OrderParam.py

--- OrderParam.py
+++ OrderParam.py
@@ -2,21 +2,29 @@
 
 # p1 ,p2 stands for the order of  sequence of postions to be taken under certain strategy
 
-def OrderParam(strategy_name ,signal):
+def OrderParam(strategy_name,signal):
     OrderPar  = None
     if strategy_name=='3EMA':
         # directional option selling strategy when upside movement is expected
-        if signal==1:
-            p1 ={'opt':'CE', 'step':-3,'transtype':'BUY','Qty':100}
-            p2 ={'opt': 'PE', 'step': -1, 'transtype': 'SELL','Qty': 100}
-            OrderPar = {'p1':p1,'p2':p2}
-        elif signal==-1:
-            p1 = {'opt': 'CE', 'step': 1, 'transtype': 'SELL', 'Qty': 100}
-            p2 = {'opt': 'PE', 'step': 3, 'transtype': 'BUY', 'Qty': 100}
-            OrderPar = {'p1': p2, 'p2': p1}
+        if signal == 1:
+            p1 = {'opt':'CE', 'step':-3,'transtype':'BUY','Qty':50}
+            OrderPar = {'p1':p1}
 
-    elif strategy_name=='RSI':
-        pass
+    elif strategy_name =='15_119_MA':
+        if signal == 1:
+            p1 = {'opt': 'CE', 'step': -3, 'transtype': 'BUY', 'Qty': 50}
+            OrderPar = {'p1': p1}
+    elif strategy_name =='MA_long_cross':
+        if signal == 1:
+            p1 = {'opt': 'CE', 'step': -3, 'transtype': 'BUY', 'Qty': 15}
+            OrderPar = {'p1': p1}
 
+    elif strategy_name == 'Mean_Rev_BNF':
+        if signal == 1:
+            p1 = {'opt': 'CE', 'step': -3, 'transtype': 'BUY', 'Qty': 15}
+            OrderPar = {'p1': p1}
+        elif signal == -1:
+            p1 = {'opt': 'PE', 'step': -3, 'transtype': 'BUY', 'Qty': 15}
+            OrderPar = {'p1': p1}
 
     return OrderPar
