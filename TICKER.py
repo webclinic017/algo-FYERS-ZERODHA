f6bbea7c316f7ae405d694e38ca0c05521b81ae3
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2023-10-25 12:43:19 +0530

    Update TICKER.py

--- TICKER.py
+++ TICKER.py
@@ -4,7 +4,7 @@ import pytz
 import schedule
 
 
-class TICKER_():
+class TICKER_:
     BROKER_OBJ = None
     STRATEGY_RUN = None
 
@@ -12,11 +12,12 @@ class TICKER_():
         self.ticker_under_strategy = ticker
         self.time_zone = pytz.timezone('Asia/Kolkata')
         self.ticker_space = {}
+        self.schedule_at = 5
         self.scheduler = schedule.Scheduler()
         self.update_tag = False
+        self.hist_df = None
 
-
-    def get_history(self ,symbol, interval, days = 4 ):
+    def get_history(self,symbol, interval, days=4):
         range_to = datetime.now(self.time_zone).date()
         range_from = range_to - timedelta(days=days)
         req = {"symbol": symbol, "resolution": f"{interval}", "date_format": "1", "range_from": str(range_from),
@@ -30,35 +31,27 @@ class TICKER_():
         return self.hist_df
 
     def run_update(self):
-            for ticker,interval in self.ticker_under_strategy.items():
-                self.ticker_space[f"{ticker}_{interval}"] = self.get_history(ticker,interval)
+        for ticker,interval in self.ticker_under_strategy.items():
+            self.ticker_space[f"{ticker}"] = self.get_history(ticker,interval)
 
-            for strategy in self.STRATEGY_RUN.keys():
-                self.STRATEGY_RUN[strategy].monitor_signal(self.ticker_space)
-
+        for strategy in self.STRATEGY_RUN.keys():
+            self.STRATEGY_RUN[strategy].monitor_signal()
 
     def run_scheduler(self):
         if not self.scheduler.jobs:
             now = datetime.now(self.time_zone)
-            for interval in self.ticker_under_strategy.values():
-                if now.minute % interval == 0 and now.second > 5 and now.second < 8:
-                    self.scheduler.every(interval).minutes.do(self.run_update)
-
+            if now.minute % self.schedule_at == 0 and now.second > 5 and now.second < 8:
+                self.scheduler.every(self.schedule_at).minutes.do(self.run_update)
+                print(f'task scheduled:{self.scheduler.jobs}')
 
         self.scheduler.run_pending()
 
-
-
-
-    def get_data(self ,symbol):
+    def get_data(self,symbol,interval):
         try:
-           return self.ticker_space[symbol]
+            resample = self.ticker_space[symbol].resample(f'{interval}', origin='2017-01-02 09:15').agg(
+                    {'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last', 'volume': 'sum'}).dropna()
+            return resample.iloc[:-1]
         except KeyError:
             return pd.DataFrame()
-
-
-
-
-
 
 
