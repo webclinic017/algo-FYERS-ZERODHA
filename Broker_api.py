90a086ddb208fd8f7ed03e97638b13a87da5a822
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2024-01-09 14:17:58 +0530

    Update Broker_api.py

--- Broker_api.py
+++ Broker_api.py
@@ -61,7 +61,7 @@ class BROKER_API:
             feed_message = json.loads(message)
             if 'lp' in feed_message:
                 self.ltp[self.token[str(feed_message['tk'])]] = float(feed_message['lp'])
-                print(f'\r ltp:{self.ltp}', end='', flush=True)
+                
 
         self.BROKER_APP.start_websocket(socket_open_callback=socket_open, socket_close_callback=socket_close,
                               socket_error_callback=socket_error, subscription_callback=feed_data,
