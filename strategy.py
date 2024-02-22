32afa5170fc012b0d892ca2b934ba434945e0d85
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2024-02-22 10:23:55 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -86,7 +86,7 @@ class StrategyFactory(STRATEGY_REPO):
                     self.scheduler.every(5).seconds.do(self.OrderManger.Update_OpenPosition)
             else:
                 if not self.position and self.trade_flag and not self.processed_flag:
-                    self.signal = self.get_signal()
+                    self.signal = -1*self.get_signal()
                     if self.signal:
                         self.scheduler.every(5).seconds.do(self.Open_position)
                     self.processed_flag = True
